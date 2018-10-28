79bc7f4919eca20c508458c63593ba170d084ab8
Ashvin Oli <ashvinoli10@gmail.com>
2018-10-28 10:51:14 +0545

    Complete change of plans

--- extraction.py
+++ extraction.py
@@ -1,34 +1,42 @@
 import re
 
 def extract(raw_string):
-	pattern = r"[0-9]"
-	pattern_trig = r"[s,c,t,l,a][a-z]*(.*)"
+	pattern_num = r"[0-9]+\.?[0-9]*"
+	pattern_signs = r"[\+,\-,\*,%,\^,/,\(,\)]"
+	#pattern_trig = r"[s,c,t,l,a][a-z]*\(.*\)" this shit matches everything between ( and ) ie sin"("x) - cos(x")" is also matched because of the parenthesis at the ends which i have coloned
+	pattern_trig = r"[s,c,t,l,a][a-z]*\(.*?\)" # this shit only matches sin(x)
+	pattern_alpha = r"[a-z]"
 	try:
 		new_strings = raw_string.split('=')
 		new_strings = new_strings[1].split()
 	except:
 		new_strings = raw_string.split()
 	
+	single_piece = ""
 	tokens = []
-	nums = ""
-	for j in new_strings:
-		if re.match(pattern_trig,j):
-			tokens.append(j)
-			continue
-		for i in j:
-			if re.match(pattern,i) or i == ".":
-				nums = nums + i
-			else:
-				if nums != "":
-					tokens.append(nums)
-					nums = ""
-				tokens.append(i)
-	
-	if nums != "":	
-		tokens.append(nums)	
-	return tokens
+	for i in new_strings:
+		single_piece = single_piece + i;
+	#print(single_piece)
+	while len(single_piece) > 0:
+		if re.match(pattern_num,single_piece):
+			word = re.findall(pattern_num,single_piece)
+			tokens.append(word[0])
+			single_piece = single_piece[len(word[0]):]
+		elif re.match(pattern_signs,single_piece):
+			word = re.findall(pattern_signs,single_piece)
+			tokens.append(word[0])
+			single_piece = single_piece[len(word[0]):]
+		elif re.match(pattern_trig,single_piece):
+			word = re.findall(pattern_trig,single_piece)
+			#print(word[0])
+			tokens.append(word[0])
+			single_piece = single_piece[len(word[0]):]
+		elif re.match(pattern_alpha,single_piece):
+			word = re.findall(pattern_alpha,single_piece)
+			tokens.append(word[0])
+			single_piece = single_piece[len(word[0]):]
 
-
+	return tokens	
 
 text = input("PLACE:")
 tokens = extract(text)
