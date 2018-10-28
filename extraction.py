4a9da1d71341953b5a8027bb05bbb53364c157c6
Ashvin Oli <ashvinoli10@gmail.com>
2018-10-28 13:56:03 +0545

    Huge update. RPN Tokenization code has been added. Till now I see no errors

--- extraction.py
+++ extraction.py
@@ -1,11 +1,56 @@
 import re
+from collections import deque
+
+#global declaration of patterns
+pattern_num = r"[0-9]+\.?[0-9]*"
+pattern_signs = r"[\+,\-,\*,%,\^,/,\(,\)]"
+pattern_signs_no_brackets = r"[\+,\-,\*,%,\^,/]"
+#pattern_trig = r"[s,c,t,l,a][a-z]*\(.*\)" this shit matches everything between ( and ) ie sin(x) - cos(x) is also matched because of the parenthesis at the ends which i have coloned
+pattern_trig = r"[s,c,t,l,a][a-z]*\(.*?\)" # this shit only matches sin(x)
+pattern_alpha = r"[a-z]"
+
+
+def to_rpn(tokens):
+	output_queue = deque([])
+	op_stack = []
+	
+	for i in tokens:
+		if re.match(pattern_num,i) or re.match(pattern_alpha,i) or re.match(pattern_trig,i):
+			output_queue.append(i)
+		elif re.match(pattern_signs_no_brackets,i):
+			if len(op_stack)==0:
+				op_stack.append(i)
+			
+			else:
+				read_op = op_stack[len(op_stack)-1]
+				if not(read_op == ')' or read_op == '('):
+					while comp_op(read_op,i):
+						output_queue.append(op_stack.pop())
+						if len(op_stack)!=0:
+							read_op = op_stack[len(op_stack)-1]
+						else:	
+							break
+				op_stack.append(i)
+				
+		elif i=='(':
+			op_stack.append(i)
+		elif i==')':
+			read_op = op_stack[len(op_stack)-1]
+			while read_op != '(':
+				#print(op_stack)
+				output_queue.append(op_stack.pop())
+				read_op = op_stack[len(op_stack)-1]
+			op_stack.pop()
+		#print(op_stack)
+	for i in range(len(op_stack)):	
+		output_queue.append(op_stack.pop())	
+	return output_queue				
+
+def comp_op(first,second):
+	op = {'+':0, '-':1, '*':2,'/':2,'%':2,'^':3}
+	return op[first] > op[second]
 
 def extract(raw_string):
-	pattern_num = r"[0-9]+\.?[0-9]*"
-	pattern_signs = r"[\+,\-,\*,%,\^,/,\(,\)]"
-	#pattern_trig = r"[s,c,t,l,a][a-z]*\(.*\)" this shit matches everything between ( and ) ie sin"("x) - cos(x")" is also matched because of the parenthesis at the ends which i have coloned
-	pattern_trig = r"[s,c,t,l,a][a-z]*\(.*?\)" # this shit only matches sin(x)
-	pattern_alpha = r"[a-z]"
 	try:
 		new_strings = raw_string.split('=')
 		new_strings = new_strings[1].split()
@@ -49,3 +94,4 @@ def extract(raw_string):
 text = input("PLACE:")
 tokens = extract(text)
 print(tokens)
+print(to_rpn(tokens))
