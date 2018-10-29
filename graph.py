6619d0d9522f7ff3659a3dc6ce7749eb76d41687
Ashvin Oli <ashvinoli10@gmail.com>
2018-10-29 08:55:30 +0545

    Now an array can be evaluated

--- graph.py
+++ graph.py
@@ -1,4 +1,18 @@
 from extraction import evaluate_exp as e_exp
+import numpy as np
 
-a = input("input:")
-print(e_exp(a,1,2,3))
+
+#print(e_exp(a,1,2,3))
+
+
+def eval_arr(x_arr,function):
+	output = np.array([])
+	for i in x_arr:
+		output = np.append(output,e_exp(function,i))
+	return output
+
+function = input("input:")
+x = np.linspace(0,2,100)
+y = eval_arr(x,function)
+print(x)
+print(y)
